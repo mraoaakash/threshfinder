6a2e5db40af997edbd17dfeb74fafb401ec2daa5
Aakash Rao <mrao.aakash@gmail.com>
2024-01-31 18:09:50 +0530

    added scripts for data organization

---
+++ runfiles/organize_nucls.sh
@@ -0,0 +1,6 @@
+python /Users/mraoaakash/Documents/research/research-personal/threshfinder/tools/organize_nucls.py \
+    -i /Users/mraoaakash/Documents/research/research-personal/threshfinder/data/NuclsEvalSet
+
+
+python /Users/mraoaakash/Documents/research/research-personal/threshfinder/tools/extract_patches.py \
+    -b /Users/mraoaakash/Documents/research/research-personal/threshfinder
