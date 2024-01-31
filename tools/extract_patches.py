aaa6111120c1e4385cf725a2ba7327157dea4069
Aakash Rao <mrao.aakash@gmail.com>
2024-01-31 18:10:20 +0530

    added scripts for data organization

---
+++ tools/extract_patches.py
@@ -0,0 +1,73 @@
+import os 
+import cv2 
+import pandas as pd 
+import numpy as np
+import argparse
+import matplotlib.pyplot as plt
+import tqdm
+
+['AMBIGUOUS' 'nonTIL_stromal' 'other_nucleus' 'sTIL' 'tumor_any']
+
+mapping_class = {
+    'nonTIL_stromal': 0,
+    'sTIL': 1,
+    'tumor_any': 2,
+    'AMBIGUOUS': 3,
+    'other_nucleus': 3,
+}
+
+
+def extract_patches(base_path, size=314):
+    try:
+        data_df = os.path.join(base_path, 'data/nucls', 'nucls.csv')
+        data_df = pd.read_csv(data_df)
+        # print(data_df.head())
+    except:
+        raise ValueError('Dataframe not found at {}'.format(os.path.join(base_path, 'data/nucls', 'nucls.csv')))
+
+
+    if len(data_df) < 0:
+        raise ValueError('No data found in dataframe')
+    
+    data_out_path = os.path.join(base_path, 'data/nucls', 'processed')
+    im_out_path = os.path.join(data_out_path, 'images')
+    mask_out_path = os.path.join(data_out_path, 'masks')
+
+    os.makedirs(im_out_path, exist_ok=True)
+    
+    for index, row in tqdm.tqdm(data_df.iterrows(), total=len(data_df)):
+        image_path = row['image_path']
+        mask_path = row['mask_path']
+        image = cv2.imread(row['image_path'])
+        mask = pd.read_csv(row['mask_path']).rename(columns={'Unnamed: 0': 'index'})
+        mask = mask.drop(columns=['index'])
+        mask = mask.replace({'super_classification': mapping_class})
+
+        imzeros = np.zeros((image.shape[0], image.shape[1]))
+        for index, row in mask.iterrows():
+            xmin,  ymin,  xmax,  ymax, = row['xmin'], row['ymin'], row['xmax'], row['ymax']
+            imzeros[ymin:ymax, xmin:xmax] = row['super_classification']
+
+        image = image[0:size, 0:size]
+        mask = imzeros[0:size, 0:size]
+
+        # plt.figure()
+        # plt.imshow(image)
+        # plt.imshow(mask, alpha=0.2)
+        # plt.show()
+
+        cv2.imwrite(os.path.join(im_out_path, os.path.basename(image_path)), image)
+        cv2.imwrite(os.path.join(mask_out_path,os.path.basename(image_path)), mask)
+
+
+
+        # break
+        
+    pass
+
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('-b', '--base_path', type=str, help='path to base directory')
+    args = argparser.parse_args()
+    extract_patches(args.base_path)
