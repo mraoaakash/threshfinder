c596788e0f2d13295efe69637452cf3585356bab
Aakash Rao <mrao.aakash@gmail.com>
2024-05-13 17:27:05 +0530

    added code

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 SHAREPATH=/mnt/storage/aakashrao/cifsShare
 
-python scripts/make_image_list.py \
+python datatools/make_image_list.py \
     --data_dir $BASEPATH \
     --outdir /media/chs.gpu/DATA/aakash-work/PathLDM/output \
