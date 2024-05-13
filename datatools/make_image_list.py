08121361a8395cdb459b6e4ec327051bf2f12e7e
Aakash Rao <mrao.aakash@gmail.com>
2024-05-13 17:44:54 +0530

    added code

--- datatools/make_image_list.py
+++ datatools/make_image_list.py
@@ -23,7 +23,7 @@ def TCGADataset(data_dir, outdir, crop_size=256, token_num=75):
     indices = indices_test
 
     print(data_file.keys())
-    print(train['prob_tile'])
+    print(train['prob_til'])
 
 
     # for idx in tqdm.tqdm(indices):
