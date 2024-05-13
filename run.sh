8d75dade160d55052009af619c727159d2214639
Aakash Rao <mrao.aakash@gmail.com>
2024-05-13 17:34:20 +0530

    added code

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
-SHAREPATH=/mnt/storage/aakashrao/cifsShare/PathLDM/inputs/TCGA_Dataset
-
 python datatools/make_image_list.py \
-    --data_dir $BASEPATH \
+    --data_dir /mnt/storage/aakashrao/cifsShare/PathLDM/inputs/TCGA_Dataset \
     --outdir /media/chs.gpu/DATA/aakash-work/PathLDM/output \
